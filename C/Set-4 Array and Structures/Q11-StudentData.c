eea2f6e4fa548a5641cba63e835fec9f70d8c036
Rudra Ponkshe <rudra.ponkshe@gmail.com>
2024-01-21 19:53:45 +0530

    (✅Set-4 Final) Fixed minor issue

--- C/Set-4 Array and Structures/Q11-StudentData.c
+++ C/Set-4 Array and Structures/Q11-StudentData.c
@@ -5,7 +5,7 @@ struct student_record{
 };
 void main(){
     struct student_record cl1[10];
-    for(int i=0;i<2;i++){
+    for(int i=0;i<10;i++){
         printf("Enter the name of student %d: ",i+1);
         scanf("%s",cl1[i].name);
         printf("Enter branch of student %d: ",i+1);
@@ -13,7 +13,7 @@ void main(){
         printf("Enter marks of student %d: ",i+1);
         scanf("%d",&cl1[i].tot_marks);
     }
-    for(int i=0;i<2;i++){
+    for(int i=0;i<10;i++){
         printf("Student %d: %s",i+1,cl1[i].name);
         printf("\n\tBranch: %s\n\tMarks: %d\n",cl1[i].branch,cl1[i].tot_marks);
     }
